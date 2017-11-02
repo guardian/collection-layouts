84f858aa151b95b04b6f934b5fc66cc65e1e8694
Frédéric Gauchet <frederic.gauchet@theguardian.com>
2017-11-02 17:23:20 +0000

    Uses classes instead of structs

--- CollectionLayouts.playground/Contents.swift
+++ CollectionLayouts.playground/Contents.swift
@@ -2,19 +2,123 @@
 
 import UIKit
 
-struct CollectionLayoutTemplate: Codable {
-    let cards: [Slice]
+let gutter: CGFloat = 8
+
+class CollectionLayoutTemplate: Codable {
+    let slices: [Slice]
+
+    init(slices: [Slice]) {
+        self.slices = slices
+    }
+
+    func allCards() -> [Card] {
+        return columns().flatMap { $0.cards }
+    }
+
+    func columns() -> [SliceColumn] {
+        return slices.flatMap { $0.columns }
+    }
+
+    var count: Int {
+        return slices.reduce(0) { $0 + $1.count }
+    }
+
+    func spanForCardAtIndex(index: Int) -> Int {
+        var cardCount = 0
+        for column in columns() {
+            cardCount += column.cards.count
+            if (index < cardCount) {
+                return column.span
+            }
+        }
+        fatalError()
+    }
+
+    func setSize(_ size: CGSize, index: Int) {
+        var cardCount = 0;
+        for column in columns() {
+            for card in column.cards {
+                if cardCount == index {
+                    card.size = size
+                    return
+                }
+                cardCount += 1
+            }
+        }
+    }
+
+    var height: CGFloat {
+        slices.forEach { $0.performLayout() }
+        return slices.reduce(0) { $0 + $1.height } + CGFloat(slices.count - 1) * gutter
+    }
 }
 
-typealias Slice = [SliceColumn]
+class Slice: Codable {
+    let columns: [SliceColumn]
 
-struct SliceColumn: Codable {
+    init(columns: [SliceColumn]) {
+        self.columns = columns
+    }
+
+    var count: Int {
+        return columns.reduce(0) { $0 + $1.cards.count }
+    }
+
+    var height: CGFloat {
+        return columns.first?.height ?? 0
+    }
+
+    func performLayout() {
+        var current = 0
+        while current + 1 < columns.count {
+            // if a column has several cards, make sure cards next to each other have the same height
+            // TODO: do not do that for columns that have cards of different style
+            if columns[current].cards.count > 1 && columns[current].cards.count == columns[current + 1].cards.count {
+                for i in 0..<columns[current].cards.count {
+                    let maxHeight = max(columns[current].cards[i].size.height, columns[current + 1].cards[i].size.height)
+                    columns[current].cards[i].size.height = maxHeight
+                    columns[current + 1].cards[i].size.height = maxHeight
+                }
+            }
+            current += 1
+        }
+
+        let maxHeight = columns.reduce(0) { max($0, $1.height) }
+
+        // increase the size of the bottom card
+        columns.forEach {
+            if $0.height < maxHeight {
+                $0.cards.last?.size.height += maxHeight - $0.height
+            }
+        }
+    }
+}
+
+class SliceColumn: Codable {
     let offset: Int
     let span: Int
     let cards: [Card]
+
+    init(offset: Int, span: Int, cards: [Card]) {
+        self.offset = offset
+        self.span = span
+        self.cards = cards
+    }
+
+    var height: CGFloat {
+        return cards.reduce(0) { $0 + $1.size.height } + CGFloat(cards.count - 1) * gutter
+    }
 }
 
-struct Card: Codable {
+class Card: Codable {
+    init(style: CardStyle) {
+        self.style = style
+    }
+    var size: CGSize = .zero {
+        didSet {
+            size.height = max(size.height, style.minHeight)
+        }
+    }
     let style: CardStyle
 }
 
@@ -22,17 +126,39 @@ enum CardStyle: String, Codable {
     case compact
     case regular
     case mpu
+
+    var minHeight: CGFloat {
+        switch self {
+        case .compact: return 100
+        case .regular: return 200
+        case .mpu: return 400
+        }
+    }
 }
 
-var layouts: [String: [Slice]] = [:]
+var layouts: [String: CollectionLayoutTemplate] = [:]
 
-let regularSlice = [
+let regularSlice = Slice(columns: [
     SliceColumn(offset: 0, span: 6, cards: [Card(style: .regular)]),
     SliceColumn(offset: 6, span: 6, cards: [Card(style: .regular)]),
     SliceColumn(offset: 12, span: 6, cards: [Card(style: .regular)]),
     SliceColumn(offset: 18, span: 6, cards: [Card(style: .regular)])
-]
+])
 
+let fixed_small_slow_IV = CollectionLayoutTemplate(slices: [
+    regularSlice
+])
+
+fixed_small_slow_IV.setSize(CGSize(width: 200, height: 100), index: 0)
+fixed_small_slow_IV.setSize(CGSize(width: 200, height: 100), index: 1)
+fixed_small_slow_IV.setSize(CGSize(width: 200, height: 100), index: 2)
+fixed_small_slow_IV.setSize(CGSize(width: 200, height: 150), index: 3)
+
+
+let c = fixed_small_slow_IV.count
+let h = fixed_small_slow_IV.height
+
+/*
 let regularSliceLarge = [
     SliceColumn(offset: 0, span: 12, cards: [Card(style: .regular)]),
     SliceColumn(offset: 12, span: 6, cards: [Card(style: .regular)]),
@@ -192,4 +318,4 @@ layouts["dynamic/slow-mpu"] = [
         SliceColumn(offset: 12, span: 12, cards: [Card(style: .mpu)])
     ]
 ]
-
+*/
